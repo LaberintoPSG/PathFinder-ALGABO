0caedeb85792d0488291bdd8a2a61b630e918464
Rafa <filardisafa@gmail.com>
2024-11-10 17:11:12 -0300

    style fixing for square on maze

--- src/Modules/_shared/componentes/maze/square.tsx
+++ src/Modules/_shared/componentes/maze/square.tsx
@@ -20,62 +20,51 @@ export const Square: React.FC<SquareProps> = ({ wall, isStart, isEnd, extraStyle
 
     const [hasRightWall,setHasRightWall] = useState<boolean>(wall?.wall_position === 'right')
     const [hasBottomWall,setHasBottomtWall] = useState<boolean>(wall?.wall_position === 'bottom')
-    const [rowCoord, setRowCoord] = useState(coord ? coord.split("-")[0] : 0)
-    const [columnCoord, setColumnCoord] = useState(coord ? coord.split("-")[1] : 0)
 
     return (
         <div style={{
-            // border: '1px solid #999',
-            float: 'left',
+            boxSizing: 'border-box',
+            position: 'relative', 
             fontSize: '24px',
             fontWeight: 'bold',
             lineHeight: '34px',
             height: '34px',
-            marginRight: '-1px',
-            marginTop: '-1px',
-            padding: '0',
-            textAlign: 'center',
             width: '34px',
-            // backgroundColor: wall ? 'grey' : '#fff',
-            borderRight: hasRightWall ? '5px solid black' : 'none',
-            borderBottom:  hasBottomWall ? '5px solid black' : 'none',
-            // transition: 'background-color 0.5s ease',
-            // ...extraStyles,
-            maxWidth: '99%',
-            minHeight: '100%'
-            }}>
+            textAlign: 'center',
+            maxWidth: '100%',
+            minHeight: '100%',
+        }}>
             <div style={{
-                marginTop: '5px',
-                marginLeft: '5px',
-                width: hasRightWall ? 'calc(100% - 5px)' : '100%',
-                height: hasBottomWall ? 'calc(100% - 5px)': '100%',
-
-                ...extraStyles
+                borderRight: hasRightWall ? '5px solid black' : 'none',
+                borderBottom: hasBottomWall ? '5px solid black' : 'none',
+                width: '100%',
+                height: '100%',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                boxSizing: 'border-box',
             }}>
-                {
-                    isStart && (
+                <div style={{
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    ...extraStyles, //the color of the 
+                    boxSizing: 'border-box',
+                }}>
+                    {isStart && (
                         <Tooltip title="Inicio" arrow open={true} placement="top">
-                            <div style={{ display: 'inline-block' }}>
-                                <StartIcon />
-                            </div>
+                            <StartIcon />
                         </Tooltip>
-                    )
-                }
-                {
-                    isEnd && (
+                    )}
+                    {isEnd && (
                         <Tooltip title="Fin" arrow open={true}>
-                            <div style={{ display: 'inline-block' }}>
-                                <LogoutIcon />
-                            </div>
+                            <LogoutIcon />
                         </Tooltip>
-                    )
-                }
-                { isDebugEnable && (
-                    <span style={{
-                        fontSize: '7px'
-                    }}>{coord}</span>
-                )
-                }
+                    )}
+                    {isDebugEnable && <span style={{ fontSize: '7px' }}>{coord}</span>}
+                </div>
             </div>
         </div>
     )
