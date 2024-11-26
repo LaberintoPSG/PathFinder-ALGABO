ba89c37fe5dcb751cb809c86a5dbeb6e6929ce97
Rafael Filardi <filardisafa@gmail.com>
2024-11-25 21:20:22 -0300

    added simplified heuristic

--- src/Context/debug-context.tsx
+++ src/Context/debug-context.tsx
@@ -1,6 +1,8 @@
 import React, { createContext, useState, useContext, ReactNode, useEffect } from 'react';
 import { IWall } from '../Interfaces/IWall';
 import { ConverterGraphWallNotationToAdjList, ShortestPathFromAnyNodeToEnd } from '../Modules/_shared/utils';
+import prunedGraph from '../Graphs/PRUNEDGRAPH_2511_3.json'
+import { Dijkstra } from '../Algorithms/Dijkstra';
 
 interface DebugContextType {
     isDebugEnable: boolean,
@@ -22,7 +24,8 @@ interface DebugContextType {
     statusLog: string[],
     setStatusLog: (logs: string[]) => void,
     isDebugTerminalOpen: boolean,
-    setIsDebugTerminalOpen: (logs: boolean) => void
+    setIsDebugTerminalOpen: (logs: boolean) => void,
+    distancesToGoalSimplifiedMaze: { [key: string]: number }
 }
 
 const DebugContext = createContext<DebugContextType | undefined>(undefined);
@@ -46,6 +49,7 @@ export const DebugContextProvider: React.FC<DebugProviderProps> = ({ children })
     const [pathNodeCounter, setPathNodeCounter] = useState<number>(0)
     const [statusLog, setStatusLog] = useState<string[]>([])
     const [isDebugTerminalOpen, setIsDebugTerminalOpen] = useState<boolean>(false)
+    const [distancesToGoalSimplifiedMaze, setDistancesToGoalSimplifiedMaze] = useState<{ [key: string]: number }>({})
 
     const [graph, setGraph] = useState<{
         length: number;
@@ -56,6 +60,28 @@ export const DebugContextProvider: React.FC<DebugProviderProps> = ({ children })
         width: 0,
         walls: []
     })
+
+    useEffect(() => {
+        const simplifiedMaze = prunedGraph as {
+            length: number,
+            width: number,
+            walls: IWall[]
+        };
+    
+        const graph = ConverterGraphWallNotationToAdjList(simplifiedMaze);
+        const prunnedGrpahVertex = graph.getVertex();
+    
+        const distancesToGoalSimplifiedMaze: { [key: string]: number } = {};
+    
+        for (const vertex of prunnedGrpahVertex) {
+            const [xV, yV] = vertex.split('-').map(Number);
+            const _dijkstra = Dijkstra(simplifiedMaze, [xV, yV], [14, 29]);
+            distancesToGoalSimplifiedMaze[vertex] = _dijkstra.totalDistance;
+        }
+
+        setDistancesToGoalSimplifiedMaze(distancesToGoalSimplifiedMaze)
+
+    }, []);
 
     return (
         <DebugContext.Provider value={{ 
@@ -70,7 +96,8 @@ export const DebugContextProvider: React.FC<DebugProviderProps> = ({ children })
             statusLog,
             setStatusLog,
             isDebugTerminalOpen,
-            setIsDebugTerminalOpen
+            setIsDebugTerminalOpen,
+            distancesToGoalSimplifiedMaze
             }}>
             {children}
         </DebugContext.Provider>
